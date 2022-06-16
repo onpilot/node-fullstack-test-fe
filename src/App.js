7ec03909365577e2b8401657d0dad5a11424b1f6
Idan <72357530+onpilot@users.noreply.github.com>
2022-06-16 23:45:28 +0700

    feat: allow fetching Flickr feed with query string

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const path = '/api';
+    const json = 'format=json';
+
+    fetch(`${path}?${json}`)
+      .then((res) => res.json())
+      .then((data) => setData(data.data.items))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <div className='App'>
       <h1>Hello World</h1>
